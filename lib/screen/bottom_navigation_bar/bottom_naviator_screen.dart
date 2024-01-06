a744cd335ee1ee7e8924ccc9f695f47c060eadb3
Suli244 <ayilchievsulaiman@gamil.com>
2024-01-06 20:18:31 +0600

    add

--- lib/screen/bottom_navigation_bar/bottom_naviator_screen.dart
+++ lib/screen/bottom_navigation_bar/bottom_naviator_screen.dart
@@ -30,110 +30,100 @@ class _BottomNavigatorState extends State<BottomNavigatorScreen> {
       child: Scaffold(
         body: pages[index],
         extendBody: true,
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          unselectedFontSize: 0,
-          selectedFontSize: 0,
-          currentIndex: index,
-          onTap: (indexFrom) async {
-            setState(() {
-              index = indexFrom;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              label: '•',
-              icon: Image.asset(
-                AppImages.homeIcon,
-                width: 25,
-                color: AppColorsBlueScopeNews.color64717B,
-              ),
-              activeIcon: Image.asset(
-                AppImages.homeActivIcon,
-                color: AppColorsBlueScopeNews.color2D52D6,
-                width: 25,
+        bottomNavigationBar: Container(
+          height: 106,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                width: 1,
+                color: AppColorsBlueScopeNews.colorD9E6F0,
               ),
             ),
-            BottomNavigationBarItem(
-              label: '•',
-              icon: Image.asset(
-                AppImages.heartIcon,
-                width: 25,
-                color: AppColorsBlueScopeNews.color64717B,
-              ),
-              activeIcon: Image.asset(
-                AppImages.heartActivIcon,
-                color: AppColorsBlueScopeNews.color2D52D6,
-                width: 25,
-              ),
+          ),
+          child: BottomNavigationBar(
+            selectedItemColor: AppColorsBlueScopeNews.color2D52D6,
+            backgroundColor: Colors.white,
+            selectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 19,
             ),
-            BottomNavigationBarItem(
-              label: '•',
-              icon: Image.asset(
-                AppImages.editIcon,
-                width: 25,
-                color: AppColorsBlueScopeNews.color64717B,
+            unselectedFontSize: 0,
+            selectedFontSize: 0,
+            currentIndex: index,
+            onTap: (indexFrom) async {
+              setState(() {
+                index = indexFrom;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                label: index == 0 ? '•' : '',
+                icon: Image.asset(
+                  AppImages.homeIcon,
+                  width: 25,
+                  color: AppColorsBlueScopeNews.color64717B,
+                ),
+                activeIcon: Image.asset(
+                  AppImages.homeActivIcon,
+                  color: AppColorsBlueScopeNews.color2D52D6,
+                  width: 25,
+                ),
               ),
-              activeIcon: Image.asset(
-                AppImages.homeActivIcon,
-                color: AppColorsBlueScopeNews.color2D52D6,
-                width: 25,
+              BottomNavigationBarItem(
+                label: index == 1 ? '•' : '',
+                icon: Image.asset(
+                  AppImages.heartIcon,
+                  width: 25,
+                  color: AppColorsBlueScopeNews.color64717B,
+                ),
+                activeIcon: Image.asset(
+                  AppImages.heartActivIcon,
+                  color: AppColorsBlueScopeNews.color2D52D6,
+                  width: 25,
+                ),
               ),
-            ),
-            BottomNavigationBarItem(
-              label: index == 1 ? '•' : '',
-              icon: Image.asset(
-                AppImages.heartIcon,
-                width: 25,
-                color: AppColorsBlueScopeNews.color64717B,
+              BottomNavigationBarItem(
+                label: index == 2 ? '•' : '',
+                icon: Image.asset(
+                  AppImages.editIcon,
+                  width: 25,
+                  color: AppColorsBlueScopeNews.color64717B,
+                ),
+                activeIcon: Image.asset(
+                  AppImages.editActivIcon,
+                  color: AppColorsBlueScopeNews.color2D52D6,
+                  width: 25,
+                ),
               ),
-              activeIcon: Image.asset(
-                AppImages.heartActivIcon,
-                color: AppColorsBlueScopeNews.color2D52D6,
-                width: 25,
+              BottomNavigationBarItem(
+                label: index == 3 ? '•' : '',
+                icon: Image.asset(
+                  AppImages.archiveIcon,
+                  width: 25,
+                  color: AppColorsBlueScopeNews.color64717B,
+                ),
+                activeIcon: Image.asset(
+                  AppImages.archiveActivIcon,
+                  color: AppColorsBlueScopeNews.color2D52D6,
+                  width: 25,
+                ),
               ),
-            ),
-            BottomNavigationBarItem(
-              label: index == 2 ? '•' : '',
-              icon: Image.asset(
-                AppImages.editIcon,
-                width: 25,
-                color: AppColorsBlueScopeNews.color64717B,
+              BottomNavigationBarItem(
+                label: index == 4 ? '•' : '',
+                icon: Image.asset(
+                  AppImages.settingsIcon,
+                  width: 25,
+                  color: AppColorsBlueScopeNews.color64717B,
+                ),
+                activeIcon: Image.asset(
+                  AppImages.settingsActivIcon,
+                  color: AppColorsBlueScopeNews.color2D52D6,
+                  width: 25,
+                ),
               ),
-              activeIcon: Image.asset(
-                AppImages.editActivIcon,
-                color: AppColorsBlueScopeNews.color2D52D6,
-                width: 25,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: index == 3 ? '•' : '',
-              icon: Image.asset(
-                AppImages.archiveIcon,
-                width: 25,
-                color: AppColorsBlueScopeNews.color64717B,
-              ),
-              activeIcon: Image.asset(
-                AppImages.archiveActivIcon,
-                color: AppColorsBlueScopeNews.color2D52D6,
-                width: 25,
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: index == 4 ? '•' : '',
-              icon: Image.asset(
-                AppImages.settingsIcon,
-                width: 25,
-                color: AppColorsBlueScopeNews.color64717B,
-              ),
-              activeIcon: Image.asset(
-                AppImages.settingsActivIcon,
-                color: AppColorsBlueScopeNews.color2D52D6,
-                width: 25,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
